911d10295998560edc66ab34899d3d3fd6e6ff96
ChatGTHB <cihat.kose@hotmail.com>
2023-08-09 21:11:18 +0300

    MainApp updated

--- src/day01/MainApp.java
+++ src/day01/MainApp.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class MainApp {
     public static void main(String[] args) {
 
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.hepsiburada.com");
 
