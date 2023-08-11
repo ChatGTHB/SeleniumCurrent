9a760ccce0b433f3b18428411697d567b2456038
ChatGTHB <cihat.kose@hotmail.com>
2023-08-11 21:07:56 +0300

    Added OpeningWebsite class

---
+++ src/day04/_01_OpeningWebsite.java
@@ -0,0 +1,22 @@
+package day04;
+
+import utility.MyFunction;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _01_OpeningWebsite {
+    public static void main(String[] args) {
+
+        // OOP WebDriver:Interface,Parent Class        ChromeDriver,FirefoxDriver,EdgeDriver:  child class
+        WebDriver driver = new ChromeDriver(); // driver = browser
+//        WebDriver driver2= new FirefoxDriver();
+//        WebDriver driver3= new EdgeDriver();
+
+        driver.get("https://techno.study/tr"); //  open web page
+
+
+        MyFunction.wait(3);
+        //driver.close(); // closes the currently active open browser, erases from memory
+        driver.quit(); // While the program is running, it both closes and deletes all opened browsers from memory
+    }
+}
