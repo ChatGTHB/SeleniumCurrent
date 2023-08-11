440b572b98ea4103b2fab622daa98212155d6f2a
ChatGTHB <cihat.kose@hotmail.com>
2023-08-11 22:20:17 +0300

    Added _03_FindingByIdNotFoundException

---
+++ src/day04/_03_FindingByIdNotFoundException.java
@@ -0,0 +1,24 @@
+package day04;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utility.MyFunction;
+
+public class _03_FindingByIdNotFoundException {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://form.jotform.com/221934510376353"); // I went to the page
+
+        try {
+            WebElement nameBox = driver.findElement(By.id("faultylocator"));
+        } catch (Exception ex) {
+            System.out.println("Web element not found " + ex.getMessage());
+        }
+
+        MyFunction.wait(5);
+        driver.quit();
+    }
+}
