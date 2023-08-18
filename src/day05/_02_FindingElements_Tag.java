f4f569c81d4566406d1384473f1eb9127967d262
ChatGTHB <cihat.kose@hotmail.com>
2023-08-18 12:25:05 +0300

    Added _02_FindingElements_tag

---
+++ src/day05/_02_FindingElements_Tag.java
@@ -0,0 +1,25 @@
+package day05;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utility.BaseDriver;
+
+import java.util.List;
+
+public class _02_FindingElements_Tag extends BaseDriver {
+    public static void main(String[] args) {
+
+        driver.get("https://www.hepsiburada.com/");
+
+        List<WebElement> linkler = driver.findElements(By.tagName("a"));
+        // Find all elements with -a- tag
+
+        for (WebElement e : linkler) {
+            System.out.println("e.getText() = " + e.getText());
+            // break;
+        }
+
+        waitAndClose();
+    }
+}
