2e8bdf3a301627054af1f2317a86471caac3d702
ChatGTHB <cihat.kose@hotmail.com>
2023-08-18 11:36:53 +0300

    Updated _06_FindingByClassName

--- src/day04/_06_FindingByClassName.java
+++ src/day04/_06_FindingByClassName.java
@@ -6,24 +6,30 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import utility.MyFunction;
 
+import java.util.List;
+
 public class _06_FindingByClassName {
     public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
-        driver.get("https://form.jotform.com/221934510376353");
+        driver.get("https://www.hepsiburada.com/");
 
         // 1)- Find text boxes with name and fill in your first and last name
         // 2)- Find and click the button with id or name
 
-        WebElement firstName=driver.findElement(By.className("form-textbox")); // find by classname
-        // If findElement finds more than one element in the same locator, it returns the first one
-        firstName.sendKeys("Kerem");
+//        WebElement firstName=driver.findElement(By.className("form-textbox")); // find by classname
+//        // If findElement finds more than one element in the same locator, it returns the first one
+//        firstName.sendKeys("Kerem");
 
-        WebElement lastName=driver.findElement(By.name("q8_name[last]")); // find by name
-        lastName.sendKeys("Said");
+        // Reaching multiple elements according to a certain criterion
+        List<WebElement> labels= driver.findElements(By.className("sf-dod-mpQGL"));
+        for(WebElement e: labels)
+            System.out.println("e.getText() = " + e.getText());
 
-        WebElement submitButton=driver.findElement(By.id("input_2"));
-        submitButton.click();
+        // findElement fails to find element                   : no such element
+        // What does findElements do if it can't find elements ?  Returns a List with 0 elements and does not give an error
+        List<WebElement> labels2= driver.findElements(By.className("sf-dod-12345"));
+        System.out.println("labels2.size() = " + labels2.size());
 
         MyFunction.wait(5);
         driver.quit();
