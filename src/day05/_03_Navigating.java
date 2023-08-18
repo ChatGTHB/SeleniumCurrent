b71f79dc82361cff31e868f4278ad6a47cad3010
ChatGTHB <cihat.kose@hotmail.com>
2023-08-18 12:55:25 +0300

    Added _03_Navigating

---
+++ src/day05/_03_Navigating.java
@@ -0,0 +1,32 @@
+package day05;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utility.BaseDriver;
+import utility.MyFunction;
+
+public class _03_Navigating extends BaseDriver {
+    public static void main(String[] args) {
+
+        driver.navigate().to("https://testpages.herokuapp.com/styled/index.html");
+
+        MyFunction.wait(3);
+        WebElement element = driver.findElement(By.id("alerttest"));
+        element.click();                             // It went to the new page.
+        MyFunction.wait(3);
+
+        System.out.println(driver.getCurrentUrl());  // Returns the URL you are currently in.
+
+        driver.navigate().back();
+        MyFunction.wait(3);
+        System.out.println(driver.getCurrentUrl());  // Returns the URL you are currently in.
+
+        driver.navigate().forward();
+        MyFunction.wait(3);
+        System.out.println(driver.getCurrentUrl());  // Returns the URL   you are currently in.
+        System.out.println(driver.getTitle());       // Returns the TITLE you are currently in.
+
+        waitAndClose();
+    }
+}
