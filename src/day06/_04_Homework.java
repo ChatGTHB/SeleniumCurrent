a2ccd38067166355c1da910a8f36d4debcf33cb2
ChatGTHB <cihat.kose@hotmail.com>
2023-08-20 16:14:51 +0300

    Added _04_Homework

---
+++ src/day06/_04_Homework.java
@@ -0,0 +1,60 @@
+package day06;
+
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utility.BaseDriver;
+import utility.MyFunction;
+
+public class _04_Homework extends BaseDriver {
+
+    // 1- Open https://formsmarts.com/form/yu?mode=h5
+    // 2- Checked the Business.
+    // 3- Click on discover XYZ and select Online Advertising
+    // 4- Choose every day
+    // 5- Choose good
+    // 6- Click using XYZ and choose option 3
+    // Css selector
+    // Put MyFunction.wait(2); between each click vs action
+
+
+    @Test
+    public void FormTest() {
+
+        driver.get("https://formsmarts.com/form/yu?mode=h5");
+
+        MyFunction.wait(2);
+        WebElement business = driver.findElement(By.cssSelector("[type='radio'][value='Business']"));
+        business.click();
+
+        MyFunction.wait(2);
+        WebElement dropDownDiscover = driver.findElement(By.cssSelector("select[id$='_4588']"));
+        dropDownDiscover.click();
+
+        MyFunction.wait(2);
+        WebElement optionOnlineAdvertising = driver.findElement(By.cssSelector("option[value='Online Advertising']"));
+        optionOnlineAdvertising.click();
+
+        MyFunction.wait(2);
+        WebElement everyDayRadio = driver.findElement(By.cssSelector("[type='radio'][value='Every Day']"));
+        everyDayRadio.click();
+
+        MyFunction.wait(2);
+        WebElement goodRadio = driver.findElement(By.cssSelector("[type='radio'][value='Good']"));
+        goodRadio.click();
+
+        MyFunction.wait(2);
+        WebElement dropdownHowLong = driver.findElement(By.cssSelector("select[id$='_4597']"));
+        dropdownHowLong.click();
+
+        MyFunction.wait(2);
+        WebElement option3 = driver.findElement(By.cssSelector("select[id$='_4597']> :nth-child(3)"));
+        option3.click();
+
+        MyFunction.wait(2);
+        goodRadio.click();
+
+        waitAndClose();
+    }
+}
