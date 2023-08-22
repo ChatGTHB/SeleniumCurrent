bcfdcf4fdb34205c6c45fda3a072d3df92fdbbab
ChatGTHB <cihat.kose@hotmail.com>
2023-08-22 14:25:05 +0300

    Added _08_ActionHoverTest

---
+++ src/day08/_08_ActionHoverTest.java
@@ -0,0 +1,29 @@
+package day08;
+
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import utility.BaseDriver;
+import utility.MyFunction;
+
+public class _08_ActionHoverTest extends BaseDriver {
+
+    @Test
+    public void Test1() {
+
+        driver.get("https://www.hepsiburada.com/");
+
+        WebElement element = driver.findElement(By.xpath("//span[text()='Moda']"));
+        MyFunction.wait(2);
+
+        Actions actions = new Actions(driver);
+        Action action = actions.moveToElement(element).build(); // hover over element
+        action.perform(); // enable it.
+
+        MyFunction.wait(2);
+        waitAndClose();
+    }
+}
