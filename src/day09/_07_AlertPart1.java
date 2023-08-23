baa47764d250f2017b2086864036b2e2d604c52f
ChatGTHB <cihat.kose@hotmail.com>
2023-08-23 23:09:41 +0300

    Added _07_AlertPart1

---
+++ src/day09/_07_AlertPart1.java
@@ -0,0 +1,29 @@
+package day09;
+
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utility.BaseDriver;
+import utility.MyFunction;
+
+public class _07_AlertPart1 extends BaseDriver {
+
+    @Test
+    public void Test() {
+
+        driver.get("http://demo.seleniumeasy.com/javascript-alert-box-demo.html");
+        MyFunction.wait(3);
+
+        WebElement clickMe = driver.findElement(By.cssSelector("button[onclick='myAlertFunction()']"));
+        clickMe.click();
+
+        MyFunction.wait(2);
+
+        // alert opened.
+        driver.switchTo().alert().accept();
+        // Go to the alert box and press OK. so it closes.
+
+        waitAndClose();
+    }
+}
