009d6441f3af48fa4f6fbd3bcc34c591c0021c9a
ChatGTHB <cihat.kose@hotmail.com>
2023-08-24 13:34:30 +0300

    Added _03_ExplicitWait

---
+++ src/day10/_03_ExplicitWait.java
@@ -0,0 +1,61 @@
+package day10;
+
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utility.BaseDriver;
+
+import java.time.Duration;
+
+
+public class _03_ExplicitWait extends BaseDriver {
+
+    // ExplicitWait
+    @Test
+    public void Test() {
+
+        driver.get("http://seleniumpractise.blogspot.com/2016/08/how-to-use-explicit-wait-in-selenium.html");
+
+        WebElement button = driver.findElement(By.xpath("//button[@onclick='timedText()']"));
+        button.click();
+
+        // Wait until the text is "Web Driver"
+        // In this case, it is necessary to give special criteria to the element.
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5)); // Defined once
+        // Just like Duration, only time and driver were defined.
+        // wait.until(ExpectedConditions.textToBe(By.id("demo"), "WebDriver"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[text()='WebDriver']")));
+        // visibilityOfElementLocated : WAIT until this locator appears.
+
+        WebElement message = driver.findElement(By.id("demo"));
+        System.out.println("message.getText() = " + message.getText());
+
+        waitAndClose();
+    }
+
+    /**
+     Mülakat Sorusu : Implicit ve Explicit
+
+     Implicit: Bütün elemanlar için geçerli olup; bir kere tanımlanır. (HTML de verilen locator var mı?)
+     Explicit: Tek bir elemana özel kriter olarak tanımlanır ve sadece ilgili elemanda geçerli olur.
+
+     Her ikisi de tanımlı ise hangisinin süresi kullanılır ?
+     önce Implicit kullanılır, ve üzerine Explicit'in süresi eklenerek
+     toplam hata sınırı süresi (mühlet sınırı) ortaya çıkmış olur.
+
+
+     ===================****************************************======================================
+
+     Interview Question: Implicit and Explicit
+
+     Implicit: It is valid for all elements; defined once. (Is there a locator given in HTML?)
+     Explicit: It is defined as a criterion specific to a single element and is valid only for the relevant element
+
+     If both are defined, whose duration is used?
+     Implicit is used first, with the duration of Explicit added to it
+     the total error limit time (delay limit) will appear.
+     */
+}
