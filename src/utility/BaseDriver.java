3d454a677f038ccf0f112513111833fae8dc0b1e
ChatGTHB <cihat.kose@hotmail.com>
2023-08-24 12:40:00 +0300

    Updated BaseDriver: Added logger and wait

--- src/utility/BaseDriver.java
+++ src/utility/BaseDriver.java
@@ -2,18 +2,27 @@ package utility;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class BaseDriver {
 
     public static WebDriver driver; // SingletonDriver method
+    public static WebDriverWait wait;
 
     static {  // The condition of this is that it is extends and takes place in the first place.
+
+        Logger logger = Logger.getLogger(""); // Get output logs.
+        logger.setLevel(Level.SEVERE);             // Show only ERRORs
+
         driver = new ChromeDriver();
         driver.manage().window().maximize(); // It maximizes the screen.
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30)); // 30 sec delay: time to load the page
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));  // 30 sec delay: time to find the element
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
     }
 
     public static void waitAndClose() {
