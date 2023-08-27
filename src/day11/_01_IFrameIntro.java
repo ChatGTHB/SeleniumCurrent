8e480e9e7563716e876db2f4a6eb341f9e0e3c72
ChatGTHB <cihat.kose@hotmail.com>
2023-08-27 12:45:49 +0300

    Added _01_IFramesIntroFrames.png

---
+++ src/day11/_01_IFrameIntro.java
@@ -0,0 +1,37 @@
+package day11;
+
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utility.BaseDriver;
+
+public class _01_IFrameIntro extends BaseDriver {
+
+    @Test
+    public void Test() {
+
+        driver.get("https://chercher.tech/practice/frames");
+        long startTime = System.currentTimeMillis();
+
+        //driver.switchTo().frame(0);  // It can be passed by giving 24 indexes: index on the page
+        //driver.switchTo().frame("frame1"); // You can pass by giving id 20112, but it's slow
+        //driver.switchTo().frame("iamframe"); // It can also be passed by giving 163 names, but it's fast
+        WebElement frame1 = driver.findElement(By.id("frame1"));
+        driver.switchTo().frame(frame1); // You can also export it as 71 weblements, but it's fast.
+        // index>webelement>name>id --> Depends on the selenium version.
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Süre = " + (endTime - startTime));
+
+        WebElement txtBox = driver.findElement(By.cssSelector("[id='topic']+input"));
+        txtBox.sendKeys("Türkiye");
+
+        // If a locator in an iframe will be processed, I need to switch to that iframe first.
+        // driver.switchTo().frame(0);      // switch to relevant room (iframe)
+        // driver.switchTo().parentFrame(); // go to the previous page, one back
+        driver.switchTo().defaultContent(); // goes directly to the outermost page
+
+        waitAndClose();
+    }
+}
