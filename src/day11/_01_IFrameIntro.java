6cd68c3eab8f02906b4c6581c600b29ee175a552
ChatGTHB <cihat.kose@hotmail.com>
2023-08-27 16:45:31 +0300

    Updated _01_IFramesIntroFrames.png

--- src/day11/_01_IFrameIntro.java
+++ src/day11/_01_IFrameIntro.java
@@ -12,20 +12,22 @@ public class _01_IFrameIntro extends BaseDriver {
     public void Test() {
 
         driver.get("https://chercher.tech/practice/frames");
+
         long startTime = System.currentTimeMillis();
 
-        //driver.switchTo().frame(0);  // It can be passed by giving 24 indexes: index on the page
-        //driver.switchTo().frame("frame1"); // You can pass by giving id 20112, but it's slow
-        //driver.switchTo().frame("iamframe"); // It can also be passed by giving 163 names, but it's fast
+        //driver.switchTo().frame(0);          // It can be passed by giving indexes: index on the page
+        //driver.switchTo().frame("frame1");   // You can pass by giving id, but it's slow
+        //driver.switchTo().frame("iamframe"); // It can also be passed by giving name, but it's fast
+
         WebElement frame1 = driver.findElement(By.id("frame1"));
-        driver.switchTo().frame(frame1); // You can also export it as 71 weblements, but it's fast.
-        // index>webelement>name>id --> Depends on the selenium version.
+        driver.switchTo().frame(frame1); // You can also export it as weblement, but it's fast.
+                                         // index>webelement>name>id --> Depends on the selenium version.
 
         long endTime = System.currentTimeMillis();
-        System.out.println("Süre = " + (endTime - startTime));
+        System.out.println("Duration = " + (endTime - startTime));
 
-        WebElement txtBox = driver.findElement(By.cssSelector("[id='topic']+input"));
-        txtBox.sendKeys("Türkiye");
+        WebElement textBox = driver.findElement(By.cssSelector("[id='topic']+input"));
+        textBox.sendKeys("Türkiye");
 
         // If a locator in an iframe will be processed, I need to switch to that iframe first.
         // driver.switchTo().frame(0);      // switch to relevant room (iframe)
