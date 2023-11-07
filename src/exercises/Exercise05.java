0c83581172a35c76d53ba339667b1d9a46a55854
ChatGTHB <cihat.kose@hotmail.com>
2023-11-07 18:37:48 +0100

    Updated Exercise05.java

--- src/exercises/Exercise05.java
+++ src/exercises/Exercise05.java
@@ -1,5 +1,6 @@
 package exercises;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -9,37 +10,40 @@ import utility.BaseDriver;
 public class Exercise05 extends BaseDriver {
 
     /**
-     Scenario:
-     1- Open the site https://www.demoblaze.com/index.html.
-     2- Click on the Samsung galaxy s6 link.
-     3- Add to cart.
-     4- Then click on PRODUCT STORE to return to the main screen.
-
-     ==============================*********************=======================
-
-     Senaryo:
-     1- https://www.demoblaze.com/index.html siteyi açınız.
-     2- Samsung galaxy s6 linkine tıklayınız.
-     3- Sepete ekleyiniz.
-     4- Daha Sonra PRODUCT STORE yazısına tıklatarak ana ekrana geri dönünüz.
+     * Scenario:
+     * 1- Open the site https://www.demoblaze.com/index.html.
+     * 2- Click on the Samsung galaxy s6 link.
+     * 3- Add to cart.
+     * 4- Then click on PRODUCT STORE to return to the main screen.
+     * <p>
+     * ==============================*********************=======================
+     * <p>
+     * Senaryo:
+     * 1- https://www.demoblaze.com/index.html siteyi açınız.
+     * 2- Samsung galaxy s6 linkine tıklayınız.
+     * 3- Sepete ekleyiniz.
+     * 4- Daha Sonra PRODUCT STORE yazısına tıklatarak ana ekrana geri dönünüz.
      */
 
     @Test
-    public void test(){
+    public void test() {
 
         driver.get("https://www.demoblaze.com/index.html");
 
-        WebElement link=driver.findElement(By.linkText("Samsung galaxy s6"));
+        WebElement link = driver.findElement(By.linkText("Samsung galaxy s6"));
         link.click();
 
-        WebElement addToCart=driver.findElement(By.linkText("Add to cart"));
+        WebElement addToCart = driver.findElement(By.linkText("Add to cart"));
         addToCart.click();
 
         wait.until(ExpectedConditions.alertIsPresent()); // wait until alert appears
         driver.switchTo().alert().accept();
 
-        WebElement webpage=driver.findElement(By.id("nava"));
+        WebElement webpage = driver.findElement(By.id("nava"));
         webpage.click();
+
+        wait.until(ExpectedConditions.urlToBe("https://www.demoblaze.com/index.html"));
+        Assert.assertEquals("The page did not return to the mainpage", driver.getCurrentUrl(), "https://www.demoblaze.com/index.html");
 
         waitAndClose();
     }
