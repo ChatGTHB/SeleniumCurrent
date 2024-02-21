dfddc3a144e908e032538f390281b96c2ea1df74
ChatGTHB <cihat.kose@hotmail.com>
2024-02-21 15:19:19 +0100

    Edited _05_FindingByClassName.java

--- src/day04/_05_FindingByClassName.java
+++ src/day04/_05_FindingByClassName.java
@@ -8,27 +8,26 @@ import utility.MyFunction;
 
 import java.util.List;
 
-public class _06_FindingByClassName {
+public class _05_FindingByClassName {
     public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.hepsiburada.com/");
-
-        // 1)- Find text boxes with name and fill in your first and last name
-        // 2)- Find and click the button with id or name
 
 //        WebElement firstName=driver.findElement(By.className("form-textbox")); // find by classname
 //        // If findElement finds more than one element in the same locator, it returns the first one
 //        firstName.sendKeys("Kerem");
 
         // Reaching multiple elements according to a certain criterion
-        List<WebElement> labels= driver.findElements(By.className("sf-dod-mpQGL"));
-        for(WebElement e: labels)
+        List<WebElement> labels = driver.findElements(By.className("sf-dod-mpQGL"));
+        for (WebElement e : labels) {
             System.out.println("e.getText() = " + e.getText());
+        }
+
 
         // findElement fails to find element                   : no such element
         // What does findElements do if it can't find elements ?  Returns a List with 0 elements and does not give an error
-        List<WebElement> labels2= driver.findElements(By.className("sf-dod-12345"));
+        List<WebElement> labels2 = driver.findElements(By.className("sf-dod-12345"));
         System.out.println("labels2.size() = " + labels2.size());
 
         MyFunction.wait(5);
