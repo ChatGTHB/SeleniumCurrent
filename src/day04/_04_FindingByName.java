c96b3f44f88bb7ee2bed660bfc1d0b95aa3bc736
ChatGTHB <cihat.kose@hotmail.com>
2024-02-21 15:14:19 +0100

    Edited _04_FindingByName.java

--- src/day04/_04_FindingByName.java
+++ src/day04/_04_FindingByName.java
@@ -6,22 +6,22 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import utility.MyFunction;
 
-public class _05_FindingByName {
+public class _04_FindingByName {
     public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://form.jotform.com/221934510376353");
 
-        // 1)- Find text boxes with name and fill in your first and last name
-        // 2)- Find and click the button with id or name
+        // 1)- Find text boxes with name and last name then fill in your first and last name
+        // 2)- Find and click the button with name
 
-        WebElement firstName=driver.findElement(By.name("q8_name[first]")); // find by name
+        WebElement firstName = driver.findElement(By.name("q8_name[first]")); // find by name
         firstName.sendKeys("Kerem");
 
-        WebElement lastName=driver.findElement(By.name("q8_name[last]")); // find by name
+        WebElement lastName = driver.findElement(By.name("q8_name[last]")); // find by name
         lastName.sendKeys("Said");
 
-        WebElement submitButton=driver.findElement(By.id("input_2"));
+        WebElement submitButton = driver.findElement(By.id("input_2"));
         submitButton.click();
 
         MyFunction.wait(5);
