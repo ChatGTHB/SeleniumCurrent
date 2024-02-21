1ce710b7b08fbc3c7f8dd99d9c1941f01cc0bd49
ChatGTHB <cihat.kose@hotmail.com>
2024-02-21 15:07:16 +0100

    Edited _03_WebElementGetText.java

--- src/day04/_03_WebElementGetText.java
+++ src/day04/_03_WebElementGetText.java
@@ -6,17 +6,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import utility.MyFunction;
 
-public class _04_WebElementGetText {
+public class _03_WebElementGetText {
     public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://form.jotform.com/221934510376353");
 
-        WebElement  labelFirstName=driver.findElement(By.id("sublabel_8_first"));
+        WebElement labelFirstName = driver.findElement(By.id("sublabel_8_first"));
         System.out.println("labelFirstName.getText() = " + labelFirstName.getText());
         // getText()  -> Returns the visible text of the WebElement.
 
-        WebElement  labelLastName=driver.findElement(By.id("sublabel_8_last"));
+        WebElement labelLastName = driver.findElement(By.id("sublabel_8_last"));
         System.out.println("labelLastName.getText() = " + labelLastName.getText());
 
         MyFunction.wait(5);
