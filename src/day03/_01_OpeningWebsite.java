570e03bd11a120568671c422ded7ca2e3cfa91d3
ChatGTHB <cihat.kose@hotmail.com>
2024-02-21 15:18:09 +0100

    Edited

--- src/day03/_01_OpeningWebsite.java
+++ src/day03/_01_OpeningWebsite.java
@@ -8,37 +8,22 @@ import utility.MyFunction;
 public class _01_OpeningWebsite {
     public static void main(String[] args) {
 
-        WebDriver driver=new ChromeDriver();
-                       //new FirefoxDriver();
-                       //new EdgeDriver();
+        // OOP WebDriver:Interface,Parent Class        ChromeDriver,FirefoxDriver,EdgeDriver:  child class
+        WebDriver driver = new ChromeDriver(); // driver = browser
+        //        WebDriver driver2= new FirefoxDriver();
+        //        WebDriver driver3= new EdgeDriver();
 
-        driver.get("https://techno.study/tr");  // open web page
+        driver.get("https://techno.study/tr"); //  open web page
 
 
         MyFunction.wait(3);
-        // waiting
-        driver.quit();
+        //driver.close(); // closes the currently active open browser, erases from memory
+        driver.quit(); // While the program is running, it both closes and deletes all opened browsers from memory
     }
-
-    public static class _01_OpeningWebsite {
-        public static void main(String[] args) {
-
-            // OOP WebDriver:Interface,Parent Class        ChromeDriver,FirefoxDriver,EdgeDriver:  child class
-            WebDriver driver = new ChromeDriver(); // driver = browser
-    //        WebDriver driver2= new FirefoxDriver();
-    //        WebDriver driver3= new EdgeDriver();
-
-            driver.get("https://techno.study/tr"); //  open web page
-
-
-            MyFunction.wait(3);
-            //driver.close(); // closes the currently active open browser, erases from memory
-            driver.quit(); // While the program is running, it both closes and deletes all opened browsers from memory
-        }
 
     /**
      * To remove the redness that is not an error but occurs when you run:
      * org.slf4j:slf4j-nop:1.7.30    ---> Add to project library from maven repository
      */
-    }
 }
+
