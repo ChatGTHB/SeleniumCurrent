6e91c546593ffb3338ba4cd69812e1489d6e8c82
ChatGTHB <cihat.kose@hotmail.com>
2024-03-09 10:35:29 +0100

    Edited

--- src/day14/_01_B_ShadowRoot.java
+++ src/day14/_01_B_ShadowRoot.java
@@ -20,43 +20,5 @@ public class _01_B_ShadowRoot extends BaseDriver {
         usernameField.sendKeys("username");
 
         waitAndClose();
-
-//        WebElement shadowHost = driver.findElement(By.xpath("(//dxbl-input-editor)[1]"));
-//        SearchContext shadowRoot=shadowHost.getShadowRoot();
-//
-//        WebElement usernameField = shadowRoot.findElement(By.xpath("(//dxbl-input-editor)[1]/input"));
-//        wait.until(ExpectedConditions.elementToBeClickable(usernameField));
-//        usernameField.sendKeys("username");
-//
-//        waitAndClose();
-
-
-//        WebElement shadowHost = driver.findElement(By.xpath("(//*[@class='dxbl-col-md-12 dxbl-col dxbl-fl-item'])[2]"));
-//
-//
-//        WebElement shadowHost2 =shadowHost.findElement(By.cssSelector("#idd506e729-314a-4ff0-b9d2-122af1d0c916"));
-//        SearchContext secondShadowRoot=shadowHost2.getShadowRoot();
-//
-//        WebElement input = secondShadowRoot.findElement(By.id("id1a189413-51d3-42f3-a27f-3e0a783d3184"));
-//        input.sendKeys("ali");
-
-
-//        WebElement shadowHost = driver.findElement(By.xpath("(//*[@class='dxbl-col-md-12 dxbl-col dxbl-fl-item'])[2]"));
-//        MyFunction.wait(5);
-//        SearchContext shadowRoot1=shadowHost.getShadowRoot();
-//        MyFunction.wait(5);
-
-
-//        WebElement shadowHost2 = driver.findElement(By.className("dxbl-text-edit"));
-//        SearchContext shadowRoot2 = shadowHost2.getShadowRoot();
-//        WebElement input = shadowRoot2.findElement(By.className("dxbl-text-edit-input"));
-//        input.sendKeys("deneme");
-
-        //        MyFunction.wait(2);
-//
-//        WebElement shadowHost = driver.findElement(By.className("dxbl-text-edit"));
-//        WebElement input = shadowHost.findElement(By.className("dxbl-text-edit-input"));
-//        input.sendKeys("Kerem Said");
-
     }
 }
--- src/day14/_02_RecordScreen.java
+++ src/day14/_02_RecordScreen.java
@@ -12,6 +12,8 @@ import utility.MyFunction;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class _02_RecordScreen extends BaseDriver {
@@ -36,16 +38,72 @@ public class _02_RecordScreen extends BaseDriver {
 
             // Record Screen
 
-            TakesScreenshot ts = (TakesScreenshot) driver;           // Stage 1 --> I defined the screenshot variable
+            TakesScreenshot ts = (TakesScreenshot) driver;                  // Stage 1 --> I defined the screenshot variable
             File memoryState = ts.getScreenshotAs(OutputType.FILE);  // Stage 2 --> Screenshot taken, in memory
             // Save screenshot in file format: "get screenshot as file"
 
             // Stage 3 --> I need to save the screenshot in memory to physical file.
             // Save the screen recording in the memory, the path and the file I named
             FileUtils.copyFile(memoryState, new File("C:\\Users\\cihat\\Desktop\\Sundry\\Screenshots\\screenshot.png"));
-
-            // TODO : Do something so that each screen recording is recorded SEPARATELY.
         }
         waitAndClose();
+    }
+
+
+    // TODO : Do something so that each screen recording is recorded SEPARATELY.
+
+    /**
+     To ensure that each screen recording is saved separately,
+     you can modify your code by adding a timestamp to the file name.
+     This will make each screenshot file unique.
+     */
+    @Test
+    public void todoTest() throws IOException {
+
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        MyFunction.wait(2);
+
+        driver.findElement(By.name("username")).sendKeys("ismet");
+        driver.findElement(By.name("password")).sendKeys("1234");
+
+        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+        submit.click();
+
+        List<WebElement> errorMessage = driver.findElements(By.xpath("//*[text()='Invalid credentials']"));
+
+        if (!errorMessage.isEmpty()) { // If there is an error: I want to save a screenshot
+
+            System.out.println("There was a mistake");
+
+            // Record Screen
+
+            TakesScreenshot ts = (TakesScreenshot) driver;                 // Stage 1 --> I defined the screenshot variable
+            File memoryState = ts.getScreenshotAs(OutputType.FILE);  // Stage 2 --> Screenshot taken, in memory
+            // Save screenshot in file format: "get screenshot as file"
+
+            // Stage 3 --> I need to save the screenshot in memory to physical file.
+            // Save the screen recording in the memory, the path and the file I named
+            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String fileName = "screenshot_" + timestamp + ".png";
+            FileUtils.copyFile(memoryState, new File("C:\\Users\\cihat\\Desktop\\Sundry\\Screenshots\\" + fileName));
+        }
+        waitAndClose();
+
+        /**
+         In this updated code, I added the following lines:
+
+         String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+         String fileName = "screenshot_" + timestamp + ".png";
+
+         These lines generate a timestamp using the current date and time
+         and format it as "yyyyMMdd_HHmmss" (e.g., 20240309_153012).
+
+         Then, I concatenate the timestamp with the file name
+         to create a unique file name for each screenshot.
+
+         FileUtils.copyFile(memoryState, new File("C:\\Users\\cihat\\Desktop\\Sundry\\Screenshots\\" + fileName));
+
+         Finally, I use the updated file name when saving the screenshot file to ensure that each screen recording is recorded separately.
+         */
     }
 }
