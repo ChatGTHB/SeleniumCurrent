6d943f055d77aaae4709824a5205f27831b23096
ChatGTHB <cihat.kose@hotmail.com>
2024-07-19 20:03:02 +0200

    Edited

--- src/day03/_01_OpeningWebsite.java
+++ src/day03/_01_OpeningWebsite.java
@@ -1,20 +1,17 @@
 package day03;
 
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import utility.MyFunction;
 
 public class _01_OpeningWebsite {
     public static void main(String[] args) {
-
         // OOP WebDriver:Interface,Parent Class        ChromeDriver,FirefoxDriver,EdgeDriver:  child class
         WebDriver driver = new ChromeDriver(); // driver = browser
         //        WebDriver driver2= new FirefoxDriver();
         //        WebDriver driver3= new EdgeDriver();
 
         driver.get("https://techno.study/tr"); //  open web page
-
 
         MyFunction.wait(3);
         //driver.close(); // closes the currently active open browser, erases from memory
@@ -26,4 +23,3 @@ public class _01_OpeningWebsite {
      * org.slf4j:slf4j-nop:1.7.30    ---> Add to project library from maven repository
      */
 }
-
