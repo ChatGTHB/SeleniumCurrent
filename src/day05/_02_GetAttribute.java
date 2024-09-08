e4a22665e0da35e84063a0a55eef0ef36fda6173
ChatGTHB <cihat.kose@hotmail.com>
2024-09-08 10:09:35 +0200

    Added _02_GetAttribute class

---
+++ src/day05/_02_GetAttribute.java
@@ -0,0 +1,23 @@
+package day05;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _02_GetAttribute {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.hepsiburada.com/");
+
+        // linkText: The visible text of the link is used as a selector.
+        WebElement myOrdersLink = driver.findElement(By.linkText("Siparişlerim"));
+        System.out.println("myOrdersLink.getText() = " + myOrdersLink.getText());
+
+        // I can access the properties of the element with getAttribute (valid for all tags).
+        System.out.println("myOrdersLink.href = " + myOrdersLink.getAttribute("href"));
+        System.out.println("myOrdersLink.title = " + myOrdersLink.getAttribute("title"));
+        System.out.println("myOrdersLink.rel = " + myOrdersLink.getAttribute("rel"));
+    }
+}
